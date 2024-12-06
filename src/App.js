918214460ce9432f1da5b73a272ea50f981ef1b8
Jorgi Leus <Jorgi.Leus@axxes.com>
2024-12-06 12:22:27 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from "./components/Navbar";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+} from "react-router-dom";
+import { useState } from 'react'
+import Home from "./pages/Home";
+import Beasts from "./pages/Beasts";
+import Spells from "./pages/Spells";
 
 function App() {
+
+    const [loggedIn, setLoggedIn] = useState(false);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar loggedIn={loggedIn} setLoggedIn={setLoggedIn}/>
+      <Routes>
+        <Route
+          path="/"
+          element={<Home />} />
+        <Route
+         path="/beasts"
+         element={<Beasts />} />
+        <Route
+          path="/spells"
+          element={<Spells />}
+        />
+      </Routes>
+    </Router>
   );
 }
 
