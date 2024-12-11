99482233e35c3a47723c39c3e4ccc2afcf2facd1
jleus <jorgi.leus@persgroep.net>
2024-12-11 20:50:28 +0100

    [BUG-16] :: fix bug

--- src/components/FormData.jsx
+++ src/components/FormData.jsx
@@ -5,7 +5,7 @@ import './index.css';
 const FormData = (props) => {
     return (
         <div className="form-data-field">
-            <Form.Label htmlFor={props.name} column={}>{props.label}</Form.Label>
+            <Form.Label htmlFor={props.name} column="">{props.label}</Form.Label>
             <Form.Control id={props.name} type="text" name={props.name} value={props.value} onChange={props.action}/>
         </div>
     )
