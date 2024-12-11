410dc084404382b79c32877c4f210d9cd53f8bcc
jleus <jorgi.leus@persgroep.net>
2024-12-11 20:57:27 +0100

    [BUG-16] :: fix

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+    base: '/dnd-cards/',
     plugins: [react()],
     server: {
         // this ensures that the browser opens upon server start
