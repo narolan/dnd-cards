e3acd79de729e634ee3a807998818b79c52039a2
jleus <jorgi.leus@persgroep.net>
2024-12-18 23:34:41 +0100

    [FEATURE-28] :: Add Link to looters

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,12 +4,14 @@ import {
     NavLink,
     NavMenu,
 } from "./NavbarElements.jsx";
+import {NavDropdown} from "react-bootstrap";
+
 
 const Navbar = () => {
 
     const BASE_URL = import.meta.env.BASE_URL;
 
-    return(
+    return (
         <Nav>
             <NavMenu>
                 <NavLink to="/" className="home">
@@ -21,6 +23,9 @@ const Navbar = () => {
                 <NavLink to="/spells">
                     Spells
                 </NavLink>
+                <NavDropdown style={{color: "white", textDecoration: "none"}} title="Other Apps" id="nav-dropdown">
+                    <NavDropdown.Item href="https://smarinade.github.io/looters" target="_blank">Looters</NavDropdown.Item>
+                </NavDropdown>
             </NavMenu>
         </Nav>
     )
