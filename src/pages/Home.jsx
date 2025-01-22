a568d07bde8c97e4d806259b07b2329b1d6475ee
jleus <jorgi.leus@persgroep.net>
2025-01-22 19:12:31 +0100

    [NOTICKET] :: Legal stuff + prettify home page a bit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -61,15 +61,25 @@ const Home = () => {
     return (
         <article>
             <h1 className="title">Welcome to DnD Cards</h1>
+            <h2 className="title">Legal Information</h2>
             <section className="block">
-                <p className="text">The purpose of this application is to create cards for Dungeon & Dragons.</p>
-                <p className="text">You can use them through the application or print them from your desktop/laptop as actual cards.</p>
+                <p className="text">
+                    This work includes material taken from the System Reference Document 5.1 (“SRD 5.1”) by Wizards of the Coast LLC and available at <a href={"https://dnd.wizards.com/resources/systems-reference-document"}>https://dnd.wizards.com/resources/systems-reference-document</a>.
+                </p>
+                <p className="text">
+                    The SRD 5.1 is licensed under the Creative Commons Attribution 4.0 International License available at <a href={"https://creativecommons.org/licenses/by/4.0/legalcode"}>https://creativecommons.org/licenses/by/4.0/legalcode</a>.
+                </p>
+            </section>
+            <h2 className="title">Purpose</h2>
+            <section className="block">
+                <p className="text">The purpose of this application is to create cards for Dungeon & Dragons 5th Edition.</p>
+                <p className="text">You can use them through the (mobile) application or print them from your desktop/laptop as actual cards.</p>
                 <p className="text">All the info is stored locally, so no syncing happens between mobile and desktop.</p>
             </section>
             <section className="block" style={{ marginTop: "10px", marginBottom: "2rem" }}>
                 <h4>Export/Import</h4>
-                <p>In order to sync data between devices we currently support exporting and importing data.</p>
-                <p>Simply push the export button. This will generate a hashed version of your data. Go to your other device, paste the hashed text and press 'Import'.</p>
+                <p className="text">In order to sync data between devices we currently support exporting and importing data.</p>
+                <p className="text">Simply push the export button. This will generate a hashed version of your data. Go to your other device, paste the hashed text and press 'Import'.</p>
                 <Button
                     variant="primary"
                     onClick={getDataAsBase64}
