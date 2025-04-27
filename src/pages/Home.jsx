eecbfcdf507168a9bbf18fda24dc696a44d0fe1b
jleus <jorgi.leus@persgroep.net>
2025-04-27 23:27:29 +0200

    [NOTICKET] :: Fix homepage URLs layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -64,8 +64,9 @@ const Home = () => {
             <h2 className="title">Legal Information</h2>
             <section className="block">
                 <p className="text">
-                    This work includes material taken from the System Reference Document 5.1 (“SRD 5.1”) by Wizards of the Coast LLC and available at <a href={"https://dnd.wizards.com/resources/systems-reference-document"}>https://dnd.wizards.com/resources/systems-reference-document</a>.
-                    The SRD 5.1 is licensed under the Creative Commons Attribution 4.0 International License available at <a href={"https://creativecommons.org/licenses/by/4.0/legalcode"}>https://creativecommons.org/licenses/by/4.0/legalcode</a>.
+                    This work includes material taken from the System Reference Document 5.1 (“SRD 5.1”) by Wizards of the Coast LLC and available at <a className="text" href={"https://dnd.wizards.com/resources/systems-reference-document"}>https://dnd.wizards.com/resources/systems-reference-document</a>.
+                    <br/>
+                    The SRD 5.1 is licensed under the Creative Commons Attribution 4.0 International License available at <a className="text" href={"https://creativecommons.org/licenses/by/4.0/legalcode"}>https://creativecommons.org/licenses/by/4.0/legalcode</a>.
                 </p>
             </section>
             <h2 className="title">Purpose</h2>
